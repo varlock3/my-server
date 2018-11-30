260092d612568a2adb4bb38937dabc09b87ce7ea
varlock3 <44277067+varlock3@users.noreply.github.com>
2018-11-30 12:24:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,13 +6,13 @@ const Discord = require('discord.js'),
 
 client.on('guildMemberAdd', member => {
 
-const mohamed= member.guild.channels.get("498298209706442752");
+const mohamed= member.guild.channels.get("514112745554182148");
 
 if(!mohamed) return;
 
 if(mohamed) {
 
-setTimeout(() => mohamed.send(`**Ꮃelcome Ꭲo Ꮪerver greats**`), 2000)        
+setTimeout(() => mohamed.send(`**Ꮃelcome Ꭲo Ꮪerver  Uỉė..**`), 2000)        
 
 }
 
@@ -20,7 +20,7 @@ setTimeout(() => mohamed.send(`**Ꮃelcome Ꭲo Ꮪerver greats**`), 2000)
 
 client.on('ready',async () => {
 
-  client.channels.find(ch => ch.id === "490181348171186196" && ch.type === 'voice').join();
+  client.channels.find(ch => ch.id === "514118021552406529" && ch.type === 'voice').join();
 
 });
 
